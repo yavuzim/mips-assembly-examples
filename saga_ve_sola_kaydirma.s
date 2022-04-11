4100370f2960c933d75daa5eaadec7596bb5a1d9
Kemal Yavuz İmer <yavuz.imer133@gmail.com>
2022-04-11 18:00:18 +0300

    saga ve sola kaydirma

---
+++ saga_ve_sola_kaydirma.s
@@ -0,0 +1,22 @@
+#Sablon
+
+.data
+
+.text
+
+main:
+
+# Sola kaydırma
+
+li $t0,3
+sll $t1,$t0,2 # $t0 registerindeki 3 sayısını 2 bit sola kaydır (2 defa 2 ile çarp, 3*2=6 6*2=12 sonuç=12)
+
+# Sağa kaydırma
+
+li $t2,48 
+srl $t3,$t2,3 # $t2 registerindeki 48 sayısını 3 bit sağa kaydır (3 defa 2'ye böl', 48/2=24 24/2=12 12/2=6 sonuç=6)
+
+
+#Sonlandırma
+li $v0,10
+syscall
